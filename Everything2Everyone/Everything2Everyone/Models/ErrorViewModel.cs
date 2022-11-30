0dedf153b6b4c80cf24172995c3234f4162b988e
asparagus <gigabyte7617@proton.me>
2022-11-30 14:24:49 +0200

    Created MVC Project

---
+++ Everything2Everyone/Everything2Everyone/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Everything2Everyone.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
