019c6a1370c62485c1d88597f35f4854a519980d
asparagus <gigabyte7617@proton.me>
2023-01-10 13:32:45 +0200

    Project completed.

--- Everything2Everyone/Everything2Everyone/Models/Article.cs
+++ Everything2Everyone/Everything2Everyone/Models/Article.cs
@@ -1,9 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Everything2Everyone.Models
 {
